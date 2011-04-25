aad3ee442e9007f3b52fa47e69b4d7a0f1d3c4a2
Steven Schlansker <stevenschlansker@gmail.com>
2011-04-25 12:33:29 -0700

    Update to Jackson 1.8.0 because the 1.7.x series had some serious bugs with Map type handling which breaks the Guava module pretty seriously.
    I'm not 100% sure this is the right things to do, but at least it works like this.

--- src/main/java/com/fasterxml/jackson/module/guava/GuavaSerializers.java
+++ src/main/java/com/fasterxml/jackson/module/guava/GuavaSerializers.java
@@ -5,6 +5,12 @@ import org.codehaus.jackson.map.BeanProperty;
 import org.codehaus.jackson.map.JsonSerializer;
 import org.codehaus.jackson.map.SerializationConfig;
 import org.codehaus.jackson.map.Serializers;
+import org.codehaus.jackson.map.TypeSerializer;
+import org.codehaus.jackson.map.type.ArrayType;
+import org.codehaus.jackson.map.type.CollectionLikeType;
+import org.codehaus.jackson.map.type.CollectionType;
+import org.codehaus.jackson.map.type.MapLikeType;
+import org.codehaus.jackson.map.type.MapType;
 import org.codehaus.jackson.type.JavaType;
 
 import com.fasterxml.jackson.module.guava.deser.MultimapSerializer;
@@ -21,6 +27,47 @@ public class GuavaSerializers implements Serializers {
         if (Multimap.class.isAssignableFrom(raw)) {
             return new MultimapSerializer(config, type, property);
         }
+        return null;
+    }
+
+    // XXX: these appeared in 1.8; do we have to do anything here?
+
+    public JsonSerializer<?> findArraySerializer(SerializationConfig config,
+            ArrayType type, BeanDescription beanDesc, BeanProperty property,
+            TypeSerializer elementTypeSerializer,
+            JsonSerializer<Object> elementValueSerializer) {
+        return null;
+    }
+
+    public JsonSerializer<?> findCollectionSerializer(
+            SerializationConfig config, CollectionType type,
+            BeanDescription beanDesc, BeanProperty property,
+            TypeSerializer elementTypeSerializer,
+            JsonSerializer<Object> elementValueSerializer) {
+        return null;
+    }
+
+    public JsonSerializer<?> findCollectionLikeSerializer(
+            SerializationConfig config, CollectionLikeType type,
+            BeanDescription beanDesc, BeanProperty property,
+            TypeSerializer elementTypeSerializer,
+            JsonSerializer<Object> elementValueSerializer) {
+        return null;
+    }
+
+    public JsonSerializer<?> findMapSerializer(SerializationConfig config,
+            MapType type, BeanDescription beanDesc, BeanProperty property,
+            JsonSerializer<Object> keySerializer,
+            TypeSerializer elementTypeSerializer,
+            JsonSerializer<Object> elementValueSerializer) {
+        return null;
+    }
+
+    public JsonSerializer<?> findMapLikeSerializer(SerializationConfig config,
+            MapLikeType type, BeanDescription beanDesc, BeanProperty property,
+            JsonSerializer<Object> keySerializer,
+            TypeSerializer elementTypeSerializer,
+            JsonSerializer<Object> elementValueSerializer) {
         return null;
     }
 }
