bf6344349711a640ef6cd8910d5721f8fc2bc88a
Tatu Saloranta <tsaloranta@gmail.com>
2012-05-16 21:03:36 -0700

    Verify that [Issue-3] is solved

--- src/test/java/com/fasterxml/jackson/datatype/guava/TestMultimaps.java
+++ src/test/java/com/fasterxml/jackson/datatype/guava/TestMultimaps.java
@@ -1,10 +1,13 @@
 package com.fasterxml.jackson.datatype.guava;
 
+import java.util.Map;
+
 import org.junit.Before;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.ImmutableMultimap;
@@ -20,14 +23,7 @@ import static com.google.common.collect.TreeMultimap.create;
 public class TestMultimaps extends BaseTest
 {
     private static final String EXPECTED = "{\"false\":[false],\"maybe\":[false,true],\"true\":[true]}";
-    ObjectMapper mapper;
-
-    @Override
-    @Before
-    public void setUp()
-    {
-        mapper = mapperWithModule();
-    }
+    private final ObjectMapper MAPPER =  mapperWithModule();
 
     public void testMultimap() throws Exception
     {
@@ -38,16 +34,46 @@ public class TestMultimaps extends BaseTest
         map.put("maybe", Boolean.FALSE);
 
         // Test that typed writes work
-        assertEquals(EXPECTED, mapper.writerWithType(new TypeReference<Multimap<String, Boolean>>() {}).writeValueAsString(map));
+        assertEquals(EXPECTED, MAPPER.writerWithType(new TypeReference<Multimap<String, Boolean>>() {}).writeValueAsString(map));
 
         // And untyped too
-        String serializedForm = mapper.writeValueAsString(map);
+        String serializedForm = MAPPER.writeValueAsString(map);
 
         assertEquals(EXPECTED, serializedForm);
 
-        assertEquals(map, mapper.<Multimap<String, Boolean>>readValue(serializedForm, new TypeReference<TreeMultimap<String, Boolean>>() {}));
-        assertEquals(map, create(mapper.<Multimap<String, Boolean>>readValue(serializedForm, new TypeReference<Multimap<String, Boolean>>() {})));
-        assertEquals(map, create(mapper.<Multimap<String, Boolean>>readValue(serializedForm, new TypeReference<HashMultimap<String, Boolean>>() {})));
-        assertEquals(map, create(mapper.<Multimap<String, Boolean>>readValue(serializedForm, new TypeReference<ImmutableMultimap<String, Boolean>>() {})));
+        assertEquals(map, MAPPER.<Multimap<String, Boolean>>readValue(serializedForm, new TypeReference<TreeMultimap<String, Boolean>>() {}));
+        assertEquals(map, create(MAPPER.<Multimap<String, Boolean>>readValue(serializedForm, new TypeReference<Multimap<String, Boolean>>() {})));
+        assertEquals(map, create(MAPPER.<Multimap<String, Boolean>>readValue(serializedForm, new TypeReference<HashMultimap<String, Boolean>>() {})));
+        assertEquals(map, create(MAPPER.<Multimap<String, Boolean>>readValue(serializedForm, new TypeReference<ImmutableMultimap<String, Boolean>>() {})));
+    }
+
+    public void testMultimapIssue3() throws Exception
+    {
+        Multimap<String, String> m1 = TreeMultimap.create();
+        m1.put("foo", "bar");
+        m1.put("foo", "baz");
+        m1.put("qux", "quux");
+        ObjectMapper o = MAPPER;
+        
+        String t1 = o.writerWithType(new TypeReference<TreeMultimap<String, String>>(){}).writeValueAsString(m1);
+        Map<?,?> javaMap = o.readValue(t1, Map.class);
+        assertEquals(2, javaMap.size());
+        
+        String t2 = o.writerWithType(new TypeReference<Multimap<String, String>>(){}).writeValueAsString(m1);
+        javaMap = o.readValue(t2, Map.class);
+        assertEquals(2, javaMap.size());
+        
+        TreeMultimap<String, String> m2 = TreeMultimap.create();
+        m2.put("foo", "bar");
+        m2.put("foo", "baz");
+        m2.put("qux", "quux");
+        
+        String t3 = o.writerWithType(new TypeReference<TreeMultimap<String, String>>(){}).writeValueAsString(m2);
+        javaMap = o.readValue(t3, Map.class);
+        assertEquals(2, javaMap.size());
+   
+        String t4 = o.writerWithType(new TypeReference<Multimap<String, String>>(){}).writeValueAsString(m2);
+        javaMap = o.readValue(t4, Map.class);
+        assertEquals(2, javaMap.size());
     }
 }
